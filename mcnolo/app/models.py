a8d757a71a5d9ae4c779217fd93cf65ec6ef5d19
Juan Antonio Peregrina Peregrina <juanantonio.peregrinaperegrina@usp.ceu.es>
2024-10-06 14:00:22 +0200

    Añadir fotos a la bbdd eeen  cda  producto

--- mcnolo/app/models.py
+++ mcnolo/app/models.py
@@ -7,7 +7,8 @@ class Producto(models.Model):
     descripcion = models.TextField(blank=True, null=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     disponible = models.BooleanField(default=True)
-
+    imagen = models.ImageField(upload_to='productos/', blank=True, null=True)  # Campo para la imagen
+    
     def __str__(self):
         return self.nombre
         
