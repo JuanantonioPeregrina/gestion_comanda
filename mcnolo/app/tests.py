94187faacb6c8c5e23bb40594169c6d0321bb9be
ivan <ivan.martinezfloro@usp.ceu.es>
2024-10-13 15:00:23 +0200

    TESTS EMPEZADOS

--- mcnolo/app/tests.py
+++ mcnolo/app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+# tests.py
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Pedido
+from django.contrib.auth.models import User
+
+class PedidoModelTests(TestCase):
+
+    def setUp(self):
+        # Crear un usuario para asociar con el pedido
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_crear_pedido(self):
+        # Crear un pedido asociado al usuario
+        pedido = Pedido.objects.create(usuario=self.user, total=50.00, estado='pendiente')
+
+        # Verificar que el pedido se haya guardado correctamente
+        self.assertEqual(pedido.usuario.username, 'testuser')
+        self.assertEqual(pedido.total, 50.00)
+        self.assertEqual(pedido.estado, 'pendiente')
